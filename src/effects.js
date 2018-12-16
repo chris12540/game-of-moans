7b017c2a9df45ecaa08bf9eaf6548e8ef56f3ccb
Tyler Collier <git@tylercollier.com>
2018-12-15 21:39:15 -0700

    Add dataFetcher HOC for comparison

--- src/effects.js
+++ src/effects.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 export function useDataFetcher(url) {
@@ -15,4 +16,39 @@ export function useDataFetcher(url) {
   }, []);
   
   return [data, isLoading, error];
+}
+
+// Compare the above to the same thing using a higher order component (HOC).
+// To use, you'd need to use props, rather than the simplicity of local variables.
+function dataFetcher(url) {
+  return function(WrappedComponent) {
+    return class extends Component {
+      constructor() {
+        super();
+        this.state = {
+          data: null,
+          isLoading: true,
+          error: null
+        }
+      }
+
+      componentDidMount() {
+        axios.get(url).then(response => {
+          this.setState({ data: response.data })
+        })
+          .catch(error => this.setState({ error }))
+          .then(() => this.setState({ isLoading: false }));
+      }
+
+      render() {
+        const { data, isLoading, error } = this.state;
+
+        return <WrappedComponent
+          data={data}
+          isLoading={isLoading}
+          error={error}
+        />
+      }
+    }
+  }
 }
