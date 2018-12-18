76ca61a9da4b43783889da64104faff2faf5346e
Tyler Collier <git@tylercollier.com>
2018-12-18 00:08:58 -0700

    Hooks for Character

--- src/Character.js
+++ src/Character.js
@@ -1,46 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from './axios'
 import './Character.scss'
 
-export default class Character extends Component {
-  constructor() {
-    super()
-    this.state = {
-      loading: false,
-      data: null,
-    }
+export default function Character(props) {
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState(null)
+
+  const { id } = props
+
+  function fetchCharacter(characterId) {
+    setLoading(true)
+    axios.get('https://gameofmoans.com/api/character/' + characterId).then(response => {
+      setData(response.data)
+    }).then(() => setLoading(false))
   }
 
-  componentDidMount() {
-    const { id } = this.props
-    
-    this.fetchCharacter(id)
-  }
+  useEffect(() => {
+    fetchCharacter(id)
+  }, [id])
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.id !== this.props.id) {
-      this.fetchCharacter(this.props.id)
-    }
-  }
-
-  fetchCharacter(id) {
-    this.setState({ loading: true })
-    axios.get('https://gameofmoans.com/api/character/' + id).then(response => {
-      this.setState({ data: response.data })
-    }).then(() => this.setState({ loading: false }))
-  }
-
-  render() {
-    const { loading, data } = this.state
-
-    return (
-      <div className="character">
-        {data && <div>
-          <h1>{data.name}</h1>
-          <div><img src={data.imageUrl} alt={data.name} /></div>
-        </div>}
-        {loading && <div><em>Loading...</em></div>}
-      </div>
-    )
-  }
+  return (
+    <div className="character">
+      {data && <div>
+        <h1>{data.name}</h1>
+        <div><img src={data.imageUrl} alt={data.name} /></div>
+      </div>}
+      {loading && <div><em>Loading...</em></div>}
+    </div>
+  )
 }
