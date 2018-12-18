24961217845e7a144016effdadad6f9208824fde
Tyler Collier <git@tylercollier.com>
2018-12-18 00:01:28 -0700

    Hooks for GameOfMoansDemo

--- src/GameOfMoansDemo.js
+++ src/GameOfMoansDemo.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Character from './Character'
 import './GameOfMoansDemo.scss'
 
-export default class GameOfMoansDemo extends Component {
-  constructor() {
-    super()
-    this.state = {
-      currentId: 0
-    }
-  }
-  
-  render() {
-    const { currentId } = this.state
-    
-    return (
-      <div className="game-of-moans-demo">
-        <header>
-          <div><img src="/images/game_of_moans.png" alt="Game of Moans" /></div>
-          <div>Oh JavaScript... ¯\_(ツ)_/¯</div>
-        </header>
-        <main>
-          <div className="controller">
-            <h2>Current ID: {currentId}</h2>
-            <div className="actions">
-              <button onClick={() => this.setState({ currentId: currentId - 1 })}>-</button>
-              <button onClick={() => this.setState({ currentId: currentId + 1 })}>+</button>
-            </div>
+export default function GameOfMoansDemo() {
+  const [currentId, setCurrentId] = useState(0)
+
+  return (
+    <div className="game-of-moans-demo">
+      <header>
+        <div><img src="/images/game_of_moans.png" alt="Game of Moans" /></div>
+        <div>Oh JavaScript... ¯\_(ツ)_/¯</div>
+      </header>
+      <main>
+        <div className="controller">
+          <h2>Current ID: {currentId}</h2>
+          <div className="actions">
+            <button onClick={() => setCurrentId(currentId - 1)}>-</button>
+            <button onClick={() => setCurrentId(currentId + 1)}>+</button>
           </div>
-          {!!currentId && <Character id={currentId} />}
-        </main>
-      </div>
-    )
-  }
+        </div>
+        {!!currentId && <Character id={currentId} />}
+      </main>
+    </div>
+  )
 }
