ac07160fdcd1937ac9e92b32560fc5916ac631a8
Tyler Collier <git@tylercollier.com>
2018-12-18 00:52:15 -0700

    Add Divider with Hooks

--- src/GameOfMoansDemo.js
+++ src/GameOfMoansDemo.js
@@ -1,56 +1,48 @@
-import React, { Component, createRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Character from './Character'
 import './GameOfMoansDemo.scss'
 
-export default class GameOfMoansDemo extends Component {
-  constructor() {
-    super()
-    this.state = {
-      currentId: 0,
-      width: 250,
-    }
-    this.activateWatchMouse = this.activateWatchMouse.bind(this)
-    this.deactivateWatchMouse = this.deactivateWatchMouse.bind(this)
-    this.myRef = createRef()
-    this.setWidth = event => {
-      const left = this.myRef.current.getBoundingClientRect().left
-      this.setState({ width: event.x - left })
-    }
+export default function GameOfMoansDemo() {
+  const [currentId, setCurrentId] = useState(0)
+  const [width, setWidth] = useState(250)
+  const [isWatchingMouse, setIsWatchingMouse] = useState(false)
+  const myRef = useRef()
+  const watchMouse = event => {
+    const left = myRef.current.getBoundingClientRect().left
+    setWidth(event.x - left)
   }
 
-  activateWatchMouse() {
-    window.addEventListener('mousemove', this.setWidth)
-  }
+  useEffect(() => {
+    if (isWatchingMouse) {
+      window.addEventListener('mousemove', watchMouse)
+    }
 
-  deactivateWatchMouse() {
-    window.removeEventListener('mousemove', this.setWidth);
-  }
-  
-  render() {
-    const { currentId, width } = this.state
-    
-    return (
-      <div ref={this.myRef} className="game-of-moans-demo">
-        <header>
-          <div><img src="/images/game_of_moans.png" alt="Game of Moans" /></div>
-          <div>Oh JavaScript... ¯\_(ツ)_/¯</div>
-        </header>
-        <main>
-          <div className="controller" style={{ width }}>
-            <h2>Current ID: {currentId}</h2>
-            <div className="actions">
-              <button onClick={() => this.setState({ currentId: currentId - 1 })}>-</button>
-              <button onClick={() => this.setState({ currentId: currentId + 1 })}>+</button>
-            </div>
+    return () => {
+      window.removeEventListener('mousemove', watchMouse)
+    }
+  }, [isWatchingMouse])
+
+  return (
+    <div ref={myRef} className="game-of-moans-demo">
+      <header>
+        <div><img src="/images/game_of_moans.png" alt="Game of Moans" /></div>
+        <div>Oh JavaScript... ¯\_(ツ)_/¯</div>
+      </header>
+      <main>
+        <div className="controller" style={{ width }}>
+          <h2>Current ID: {currentId}</h2>
+          <div className="actions">
+            <button onClick={() => setCurrentId(currentId - 1)}>-</button>
+            <button onClick={() => setCurrentId(currentId + 1)}>+</button>
           </div>
-          <div
-            className="divider"
-            onMouseDown={this.activateWatchMouse}
-            onMouseUp={this.deactivateWatchMouse}
-          ></div>
-          {!!currentId && <div className="character-container"><Character id={currentId} /></div>}
-        </main>
-      </div>
-    )
-  }
+        </div>
+        <div
+          className="divider"
+          onMouseDown={() => setIsWatchingMouse(true)}
+          onMouseUp={() => setIsWatchingMouse(false)}
+        ></div>
+        {!!currentId && <Character id={currentId} />}
+      </main>
+    </div>
+  )
 }
