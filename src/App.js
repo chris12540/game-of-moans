4f4e7025648ab3eaad05936558ae7ba1411351db
Tyler Collier <git@tylercollier.com>
2018-12-18 00:58:15 -0700

    Show bug where componentWillUnmount has been forgotten

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css'
 import GameOfMoansDemo from './GameOfMoansDemo'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showDemo: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ showDemo: false }), 3000)
+  }
+
   render() {
+    const { showDemo } = this.state
     return (
       <div>
-        <GameOfMoansDemo />
+        {showDemo && <GameOfMoansDemo />}
       </div>
     )
   }
